fc5d6504eff7bac6c3ba7d3ef34500c96832330c
Noé Samaille <noe.samaille@ibm.com>
2023-01-24 11:04:23 +0000

    Adds basic data model
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

---
+++ data/data-model.sql
@@ -0,0 +1,23 @@
+CREATE TABLE "customer" (
+  "id" varchar PRIMARY KEY,
+  "name" varchar,
+  "firstName" varchar,
+  "email" varchar,
+  "phoneNumber" varchar,
+  "longDistance" varchar,
+  "international" varchar,
+  "local" varchar,
+  "dropped" varchar,
+  "payMethod" varchar,
+  "localBillType" varchar,
+  "longDistanceBillType" varchar,
+  "usage" varchar,
+  "ratePlan" varchar,
+  "gender" varchar,
+  "status" varchar,
+  "children" varchar,
+  "estIncome" varchar,
+  "carOwner" varchar,
+  "age" varchar
+);
+
